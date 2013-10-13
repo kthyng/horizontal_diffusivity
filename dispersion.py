1ad54451acea217aa39aa69acfeaa4cb523fa929
Kristen M. Thyng <kthyng@gmail.com>
2013-10-13 11:58:34 -0500

    fixing dispersion overall calc

--- dispersion.py
+++ dispersion.py
@@ -135,7 +135,7 @@ def run_dispersion():
                 # have already summed but not averaged
             else:
                 d = np.load(D2name)
-                D2_temp = d['D2']; t_temp = d['t']; nnans_temp = d['nnans'];
+                D2_temp = d['D2']; t = d['t']; nnans_temp = d['nnans'];
                 D2.append(D2_temp)
                 # D2 = np.nansum(np.vstack([D2, D2_temp]), axis=0) # keep summing up D2 values but retaining time dim and not averaging yet
                 # nnans = nnans + nnans_temp
